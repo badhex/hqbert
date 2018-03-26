99341ca6868c86ea0f906d9e21b853ab7f420c9c
Craig Citro <craig@citro.net>
2018-03-26 08:23:58 -0500

    Add default config

---
+++ config.dist.py
@@ -0,0 +1,9 @@
+class Config:
+    gapi = '123'
+    discord_channel = '123'
+    discord_token = '123'
+
+    question_bbox = (0,0,100,200)
+    answers_bbox = (0,0,100,200)
+
+    reversewords = ['not']
