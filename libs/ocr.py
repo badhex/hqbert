6356933cd7574be2e6092f525f840af4c096e6c6
Vincent Yarmoska <vyarmoska@gmail.com>
2018-03-29 00:15:44 -0500

    Removed get_text and replaced with custom Screen class

--- libs/ocr.py
+++ libs/ocr.py
@@ -9,7 +9,6 @@ if os.name == 'nt':
 else:
 	import pyscreenshot as ImageGrab
 
-from config import Config
 from multiprocessing.pool import ThreadPool
 
 pool = ThreadPool( processes=5 )
@@ -111,44 +110,3 @@ class Screen:
 
 	def show(self):
 		self.im.show()
-
-
-def get_text(kind, box, showcap=False, invert_img=False):
-	im = ImageGrab.grab( bbox=box )
-
-	if Config.debug or True:
-		contrast = ImageEnhance.Contrast( im )
-		im = contrast.enhance(2)
-
-	if invert_img:
-		im = im.convert('L')
-		im = ImageOps.invert(im)
-
-	if showcap:
-		im.show()
-
-	if kind is 'question':
-		return (" ".join( pytesseract.image_to_string( im ).split() )).replace( "— ", "" )
-	elif kind is 'answers':
-		answers = pytesseract.image_to_string( im, lang='eng', config='-psm 3' ).splitlines()
-		if not answers:
-			# fix for single character questions
-			print("Processing solo character...")
-			imgwidth, imgheight = im.size
-			count = 0
-			for i in range( 0, int(imgheight), int(imgheight/3) ):
-				if count >= 3:
-					break
-				box = (0, i, imgwidth/2, i + int(imgheight/3))
-				a = im.crop( box )
-				ans = pytesseract.image_to_string( a, lang='eng', config='-psm 10' ).splitlines()
-				if ans:
-					answers.append(ans[0])
-					count += 1
-		return list( filter( None, answers ) )
-	elif kind is 'multiline':
-		return pytesseract.image_to_string( im, lang='eng', config='-psm 3' ).splitlines()
-	else:
-		# just return the data that is in our box
-		return " ".join( pytesseract.image_to_string( im ).split() )
-
