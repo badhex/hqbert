b042f6cf6e2e54a76f1fe0c5994d0137a16af70b
Vincent Yarmoska <vyarmoska@gmail.com>
2018-04-05 11:11:39 -0500

    db code cleanup

--- libs/db.py
+++ libs/db.py
@@ -5,14 +5,12 @@ from config import Config
 
 
 def writeq(q, ans, correct, answered, qimage=None, aimage=None):
-	connection = pymysql.connect( host=Config.DBcfg.host, user=Config.DBcfg.user, password=Config.DBcfg.password, db=Config.DBcfg.db, charset=Config.DBcfg.charset, cursorclass=pymysql.cursors.DictCursor )
+	connection = pymysql.connect( host=Config.DBcfg.host, user=Config.DBcfg.user, password=Config.DBcfg.password, db=Config.DBcfg.db, charset=Config.DBcfg.charset,
+	                              cursorclass=pymysql.cursors.DictCursor )
 	try:
 		with connection.cursor() as cursor:
-			# Create a new record
 			sql = "INSERT INTO `" + Config.DBcfg.table + "` (`q`, `a1`, `a2`, `a3`, `correct`, `answered`, `qimage`, `aimage`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-			cursor.execute( sql, ( q, ans[0], ans[1], ans[2], int(correct), int(answered), base64.b64encode(qimage.tobytes()), base64.b64encode(aimage.tobytes()) ) )
-
-		# connection is not autocommit by default. So you must commit to save your changes.
+			cursor.execute( sql, (q, ans[0], ans[1], ans[2], int( correct ), int( answered ), base64.b64encode( qimage.tobytes() ), base64.b64encode( aimage.tobytes() )) )
 		connection.commit()
 	finally:
 		connection.close()
