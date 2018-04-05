4c6fb7b03f8f26c928b23b7bb0658f9356c9dc21
Vincent Yarmoska <vyarmoska@gmail.com>
2018-04-05 15:27:49 -0500

    Fixed typo and added enable/disable to db

--- config.dist.py
+++ config.dist.py
@@ -1,31 +1,31 @@
 
 class Config:
-    debug = True
-    debug_question_bbox = False
-    debug_answers_bbox = False
-    debug_nextgame_bbox = False
-    upscale_ocr = 600
+	debug = True
+	debug_question_bbox = False
+	debug_answers_bbox = False
+	debug_nextgame_bbox = False
+	upscale_ocr = 600
 
-    gapi = '123'
-    cseid = '123'
+	gapi = '123'
+	cseid = '123'
 
-    discord_channel = '123'
-    discord_token = '123'
+	discord_channel = '123'
+	discord_token = '123'
 
-    screen_detection_bbox = (950, 150, 1600, 1000)
-    question_threshhold = 3100000
+	screen_detection_bbox = (950, 150, 1600, 1000)
+	question_threshhold = 3100000
 
-    nextgame_bbox = (0,0,100,200)
-    question_bbox = (0,0,100,200)
-    answers_bbox = (0,0,100,200)
+	nextgame_bbox = (0,0,100,200)
+	question_bbox = (0,0,100,200)
+	answers_bbox = (0,0,100,200)
 
-    reversewords = ['not']
+	reversewords = ['not']
 
-    class DB:
-        host = 'localhost'
-        user = 'hqbert'
-        password = 'pass'
-        db = 'hqbert'
-        table = 'qa'
-        charset = 'utf8mb4'
-
+	class DB:
+		enabled = False
+		host = 'localhost'
+		user = 'hqbert'
+		password = 'pass'
+		db = 'hqbert'
+		table = 'qa'
+		charset = 'utf8mb4'
--- libs/db.py
+++ libs/db.py
@@ -5,12 +5,13 @@ from config import Config
 
 
 def writeq(q, ans, correct, answered, qimage=None, aimage=None):
-	connection = pymysql.connect( host=Config.DB.host, user=Config.DB.user, password=Config.DB.password, db=Config.DB.db, charset=Config.DB.charset,
-	                              cursorclass=pymysql.cursors.DictCursor )
-	try:
-		with connection.cursor() as cursor:
-			sql = "INSERT INTO `" + Config.DBcfg.table + "` (`q`, `a1`, `a2`, `a3`, `correct`, `answered`, `qimage`, `aimage`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-			cursor.execute( sql, (q, ans[0], ans[1], ans[2], int( correct ), int( answered ), base64.b64encode( qimage.tobytes() ), base64.b64encode( aimage.tobytes() )) )
-		connection.commit()
-	finally:
-		connection.close()
+	if Config.DB.enabled:
+		connection = pymysql.connect( host=Config.DB.host, user=Config.DB.user, password=Config.DB.password, db=Config.DB.db, charset=Config.DB.charset,
+		                              cursorclass=pymysql.cursors.DictCursor )
+		try:
+			with connection.cursor() as cursor:
+				sql = "INSERT INTO `" + Config.DB.table + "` (`q`, `a1`, `a2`, `a3`, `correct`, `answered`, `qimage`, `aimage`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+				cursor.execute( sql, (q, ans[0], ans[1], ans[2], int( correct ), int( answered ), base64.b64encode( qimage.tobytes() ), base64.b64encode( aimage.tobytes() )) )
+			connection.commit()
+		finally:
+			connection.close()
