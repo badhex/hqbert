309fb07147a48fd6c4bc8a71ab2656a1825a66bc
Vincent Yarmoska <vyarmoska@gmail.com>
2018-04-06 22:00:48 -0500

    Revert "OCR fix"
    This reverts commit c5d9f567aa030387b67f7b8b1090d005fd7aa891.

--- libs/ocr.py
+++ libs/ocr.py
@@ -85,19 +85,18 @@ class Screen:
 
 	def __text__(self, multiline=False, expected=3):
 		if not multiline:
-			return " ".join( pytesseract.image_to_string( self.im ).split() )  # .replace( "— ", "" )
+			return " ".join( pytesseract.image_to_string( self.im ).split() )
 		else:
-			txt = pytesseract.image_to_string( self.im, config='-psm 3' ).splitlines()
-			if not txt or len(list( filter( None, txt ) )) != expected:
+			txt = pytesseract.image_to_string( self.im, lang='eng', config='-psm 3' ).splitlines()
+			print("First pass:", list( filter( None, txt ) ))
+			if not txt and len( list( filter( None, txt ) ) ) != expected:
 				# Try enhancing the image contrast
-				contrast = ImageEnhance.Contrast( self.im )
+				retry = self.im.convert( 'L' )
+				contrast = ImageEnhance.Contrast( retry )
 				retry = contrast.enhance( 2 )
 				txt = pytesseract.image_to_string( retry, config='-psm 3' ).splitlines()
-				if not txt or len(list( filter( None, txt ) )) != expected:
-					# Try making the image B&W
-					retry = retry.convert( 'L' )
-					txt = pytesseract.image_to_string( retry, config='-psm 3' ).splitlines()
-			if not txt or len(list( filter( None, txt ) )) != expected:
+				print( "Second pass:", list( filter( None, txt ) ) )
+			if not txt and len( list( filter( None, txt ) ) ) != expected:
 				# check for single character lines
 				w, h = self.im.size
 				count = 0
@@ -110,10 +109,11 @@ class Screen:
 					if ans:
 						txt.append( ans[0] )
 						count += 1
+			print( "Final pass:", list( filter( None, txt ) ) )
 			return list( filter( None, txt ) )
 
-	def text(self, multiline=False, expected=3):
-		async = pool.apply_async( self.__text__, (multiline, expected) )
+	def text(self, multiline=False):
+		async = pool.apply_async( self.__text__, (multiline,) )
 		return async.get()
 
 	# returns the index highlighted
